696aa64e3183fa7b95882101ee8e6829d8b32777
Ivan Juric <ijuric12@gmail.com>
2016-04-29 00:55:30 +0200

    database test

--- TomorrowsLunch/Controllers/HomeController.cs
+++ TomorrowsLunch/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TommorowsLunch.Providers;
 
 namespace TomorrowsLunch.Controllers
 {
@@ -11,6 +12,12 @@ namespace TomorrowsLunch.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var p = new MealProvider();
+            var x = p.GetAllMeals();
+            foreach (var item in x)
+            {
+                ViewBag.Message += item.Name + " ";
+            }
             return View();
         }
         public ActionResult Generic()
